cdd4ccf246e417d9c98ae7c46aaef38b11cfdbf7
Rohit Rj <rohitjadaun3@gmail.com>
2018-08-17 00:26:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+import React, {Component} from 'react';
+import Main from './android/app/components/Main';
+
+export default class App extends React.Component {
+  render() {
+    return (
+                 <Main/>
+    );
+  }
+}
+
